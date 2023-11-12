54da1125a3939229a9700e3569abdcb79f01ba0d
Prikshit singh <prikshitsingh79@gmail.com>
2023-11-12 20:38:47 +0530

    Update settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,6 +2,13 @@
 
 export default function Settings(){
     return (
-        <div>Settings page comming soon</div>
+        <div className=" grid grid-cols-12">
+            <div className="col-span-5">
+                This will be text and settings side
+            </div>
+            <div className="col-span-7">
+                This will be image and username side
+            </div>
+        </div>
     );
 }
